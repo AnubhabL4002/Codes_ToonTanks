1aabb8fa76489f30969e43db569740f9247140af
Anubhab Lahiri <158812807+AnubhabL4002@users.noreply.github.com>
2025-03-17 01:58:30 +0530

    Add files via upload

--- BasePawn.cpp
+++ BasePawn.cpp
@@ -5,6 +5,7 @@
 #include "Components/CapsuleComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Projectile.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -36,4 +37,13 @@ void ABasePawn::RotateTurret(FVector LookAtTarget)
 			UGameplayStatics::GetWorldDeltaSeconds(this), 
 			18.f)
 	);
+}
+
+void ABasePawn::Fire()
+{
+	FVector Location = ProjectileSpawnPoint -> GetComponentLocation();
+	FRotator Rotation = ProjectileSpawnPoint->GetComponentRotation();
+
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	Projectile->SetOwner(this);
 }
